b80bfcc2ea1d2e726e48135751eeea9740066ce5
Julio-SC <jhver2001@gmail.com>
2025-06-02 13:16:40 -0700

    Exercise Completed

---
+++ nextjs-intro/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4 mt-10">
+      <p>&copy; {new Date().getFullYear()} Julio's Website. All rights reserved.</p>
+    </footer>
+  );
+}
